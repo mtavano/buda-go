100774c2cf532d57174513b9504cd0a71aea14ba
Marcello Tavano <chelotavano.udp@gmail.com>
2021-06-25 21:23:38 -0400

    Delete private pkg references

--- get_balances.go
+++ get_balances.go
@@ -3,47 +3,29 @@ package buda
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/blue-factory/cryptobot/pkg/exchange"
 )
 
 // GetBalances ...
-func (b *Buda) GetBalances() ([]*exchange.Balance, error) {
-	balances := make([]*exchange.Balance, 0)
-
+func (b *Buda) GetBalances() ([]*Balance, error) {
 	resource := fmt.Sprintf(accountBalanceEndpoint)
 	res, err := b.makeRequest(http.MethodGet, resource, nil, true)
 	if err != nil {
-		return balances, err
+		return nil, err
 	}
 
 	payload := &struct {
-		Balances []*rawBalance `json:"balances"`
+		Balances []*Balance `json:"balances"`
 	}{}
 	err = b.scanBody(res, payload)
 
-	for _, b := range payload.Balances {
-		balance := b.To()
-		fmt.Println(balance)
-		balances = append(balances, balance)
-	}
-
-	return balances, nil
+	return payload.Balances, nil
 }
 
-type rawBalance struct {
+type Balance struct {
 	ID                    string   `json:"id"`
 	Amount                []string `json:"amount"`
 	AvailableAmount       []string `json:"available_amount"`
 	FrozenAmount          []string `json:"frozen_amount"`
 	PendingWithdrawAmount []string `json:"pending_withdraw_amount"`
 	AccountID             int      `json:"account_id"`
-}
-
-func (rb *rawBalance) To() *exchange.Balance {
-	return &exchange.Balance{
-		Available: rb.AvailableAmount[0],
-		Wallet:    rb.ID,
-		Balance:   rb.Amount[0],
-	}
 }
